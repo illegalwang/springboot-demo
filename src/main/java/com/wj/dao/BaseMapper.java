04766db01fbdfafcb2dd3cf7df848edbcb062744
illegalwang <illegalwang@163.com>
2018-12-12 16:52:16 +0800

    first commit

---
+++ src/main/java/com/wj/dao/BaseMapper.java
@@ -0,0 +1,7 @@
+package com.wj.dao;
+
+/**
+ * Created by wj on 2018/12/12.
+ */
+public class BaseMapper {
+}
---
+++ src/main/java/com/wj/service/BaseService.java
@@ -0,0 +1,7 @@
+package com.wj.service;
+
+/**
+ * Created by wj on 2018/12/12.
+ */
+public class BaseService {
+}
